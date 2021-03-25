9df7a3fd4179cefa164d6cdecf516efcb7b95f72
mahbubsk <skmahbubkr787@gmail.com>
2021-03-26 03:05:21 +0600

    assets collected

---
+++ src/assets/Assets.js
@@ -0,0 +1,39 @@
+import logo         from './images/logo.svg';
+
+
+import home         from './icons/home.svg';    
+import voice        from './icons/voice.svg';
+import search       from './icons/search.svg';
+import menu         from './icons/menu.svg';
+import video        from './icons/video.svg';
+import grid         from './icons/grid.svg';
+import notification from './icons/notification.svg';
+import explore      from './icons/explore.svg';
+import subscription from './icons/subscription.svg';
+import library      from './icons/library.svg';
+import history      from './icons/history.svg';
+import watchlater   from './icons/watchlater.svg';
+import like         from './icons/like.svg';
+
+
+const images = {
+    logo
+}
+
+const icons = {
+    home,
+    voice,
+    search,
+    menu,
+    video,
+    grid,
+    notification,
+    explore,
+    subscription,
+    library,
+    history,
+    watchlater,
+    like
+}
+
+export {images,icons};
