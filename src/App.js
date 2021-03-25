b7e3ca3fd05006fa977ca8e279e6dfe25973b613
Islam Timir <islam.timir772@gmail.com>
2021-03-25 12:53:10 +0600

    Bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div>
-      
+      <button type="button" className="btn btn-primary">Primary</button>
     </div>
   );
 }
