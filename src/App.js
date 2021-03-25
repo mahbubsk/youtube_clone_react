8d2d2724663fc75bde9be29943b76ce0dc0bcf51
Islam Timir <islam.timir772@gmail.com>
2021-03-25 13:40:26 +0600

    requested push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
+import Components from '../src/components/Components'
+
+
+
+
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div>
-      <button type="button" className="btn btn-primary">Primary</button>
+      <Components />
+      
     </div>
   );
 }
---
+++ src/components/Components.jsx
@@ -0,0 +1,12 @@
+import Header from './header/Header'
+import Main from './main/Main'
+import Sidebar from './sidebar/Sidebar'
+
+let Components = () => {
+    return (
+        <Header />
+        <Main />
+    )
+}
+
+export default Components
